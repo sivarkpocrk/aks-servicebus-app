5448545a83aa81731450dafe3e90d2c4e7cdf926
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-04-03 11:44:24 +0100

    changed main.tf to include storage account and container creation for tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.backend_storage_account_name
-    container_name       = var.backend_container_name
-    key                  = var.backend_key
+    resource_group_name  = "MyResourceGroup"
+    storage_account_name = "akssiva"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
   required_providers {
     azurerm = {
